ce9003ba867b9d90ae1881eb00c7eb72fb438a3c
jose-fernando-1 <107815035+jose-fernando-1@users.noreply.github.com>
2025-03-26 16:23:07 -0300

    Example of schema and migrations
    Simple example of schema and migrations

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,1 +1,25 @@
--- Criar o esquema inicial do banco de dados.
+-- Create initial schema.
+
+DROP TABLE IF EXISTS Entry;
+
+CREATE TABLE IF NOT EXISTS Entry (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique identifier for each entry
+    username TEXT NOT NULL,               -- Username or email
+    password TEXT NOT NULL,               -- Encrypted password
+    notes TEXT,                           -- Optional notes
+    date_of_creation DATETIME DEFAULT CURRENT_TIMESTAMP, -- Timestamp of creation
+    date_of_last_password_update DATETIME DEFAULT CURRENT_TIMESTAMP -- Timestamp of last update
+);
+
+-- Timestamp is stored in UTC timezone for best practices.
+-- On display to the user, it should be converted to the user's timezone.
+
+CREATE TRIGGER update_date_of_last_password_update
+AFTER UPDATE OF password ON Entry
+FOR EACH ROW
+WHEN NEW.password != OLD.password
+BEGIN
+    UPDATE Entry
+    SET date_of_last_password_update = CURRENT_TIMESTAMP
+    WHERE id = OLD.id;
+END;
---
+++ migrations/03_migration.sql
@@ -0,0 +1,7 @@
+UPDATE entry
+SET password = "new_encrypted_password_1"
+Where username = "user1@example.com";
+
+UPDATE Entry
+SET password = "new_encrypted_password_2"
+Where username = "user2@example.com";
